5f525d400c042851d39d673ee32b9592522f2ab6
hysmun <throdlemassif@hotmail.com>
2018-01-20 12:26:38 +0100

    MODIF APP MEDECIN LABORANTIN PATIENT OK SAUF topic et queue et pas paypal

---
+++ ServerBillet/src/protocole/TICKMAPTYPE.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package protocole;
+
+import javafx.scene.chart.AreaChart;
+
+/**
+ *
+ * @author 'Toine
+ */
+public enum TICKMAPTYPE {
+    CONNECT, DISCONECT, ACHAT
+}
