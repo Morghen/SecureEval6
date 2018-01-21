32da3ccbb93ec447139c2de7afb4bc73c91e34ef
hysmun <throdlemassif@hotmail.com>
2018-01-21 12:26:11 +0100

    commit

--- ApplicationBillet/src/applicationbillet/ApplicationBilletLogin.java
+++ ApplicationBillet/src/applicationbillet/ApplicationBilletLogin.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import libs.libUtils;
 import protocole.TICKMAPTYPE;
 import protocole.tickmap;
 
