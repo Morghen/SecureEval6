084080485746d1f8207b6e5cd34b594a6db1a6d0
hysmun <throdlemassif@hotmail.com>
2018-01-24 16:36:26 +0100

    commit

--- ServerBillet/src/serveurPayement/serveurPayement.java
+++ ServerBillet/src/serveurPayement/serveurPayement.java
@@ -11,8 +11,15 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import libs.BDUtilities;
 import libs.TickmapClient;
 import libs.Tracable;
+import protocole.tickmap;
 
 /**
  *
@@ -24,11 +31,18 @@ public class serveurPayement extends Thread{
     private ServerSocket SSocket;
     private Tracable pere = null;
     public TickmapClient tc =null;
+    public BDUtilities uti=null;
     
     public serveurPayement(Tracable zonetxt) {
         SSocket = null;
         pere = zonetxt;
-        
+        try {
+            uti = new BDUtilities("localhost", 5500);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(serveurPayement.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(serveurPayement.class.getName()).log(Level.SEVERE, null, ex);
+        }
         try
         {
             SSocket = new ServerSocket(Port);
@@ -43,13 +57,40 @@ public class serveurPayement extends Thread{
     @Override
     public void run() {
         Running = true;
+        boolean connected =true;
         while(Running ){
             pere.Trace("ThServ : Serveur en attente");
             try
             {
                 tc = new TickmapClient(SSocket.accept());
-                
                 pere.Trace("ThServ :Serveur a recu la connexion");
+                
+                tickmap request = tc.read();
+                int idVols = 0;
+                int idClient =0;
+                StringTokenizer strTok;
+                pere.Trace("ThServ : "+request.toString());
+                if(request != null){
+                    switch(request.getType()){
+                        case PAYEMENT:
+                            strTok = new StringTokenizer(request.getMessage(),"#");
+                            idVols = Integer.parseInt(strTok.nextToken());
+                            idClient = Integer.parseInt(strTok.nextToken());
+                            uti.update("UPDATE ticket SET payer = \"Y\" WHERE idClient = "+idClient+" AND idVols = "+idVols);
+                            break;
+                        case NOTPAYEMENT:
+                            strTok = new StringTokenizer(request.getMessage(),"#");
+                            idVols = Integer.parseInt(strTok.nextToken());
+                            idClient = Integer.parseInt(strTok.nextToken());
+                            int nbrBillet = 0;
+                            ResultSet rs = uti.query("SELECT count(*) FROM ticket WHERE idClient = "+idClient+" AND idVols = "+idVols+" AND payer like 'N'");
+                            rs.next();
+                            nbrBillet = rs.getInt(1);
+                            uti.update("DELETE FROM ticket WHERE idClient = "+idClient+" AND idVols = "+idVols+" AND payer like 'N'");
+                            uti.update("UPDATE vols SET nbrDispo = nbrDispo-"+nbrBillet+" WHERE idVols = "+idVols);
+                            break;
+                    }
+                }
             }
             catch(SocketException e)
             {
@@ -58,6 +99,8 @@ public class serveurPayement extends Thread{
             catch(IOException e)
             {
                 pere.Trace("ThServ : Erreur accept : "+e);
+            } catch (SQLException ex) {
+                Logger.getLogger(serveurPayement.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
